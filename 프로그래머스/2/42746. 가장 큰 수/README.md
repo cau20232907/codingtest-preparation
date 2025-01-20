980046970172376bcfc71d41bdf3158a54f66c5b
cau20232907 <129158071+cau20232907@users.noreply.github.com>
2025-01-21 02:08:19 +0900

    [level 2] Title: 가장 큰 수, Time: 236.85 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
-메모리: 130 MB, 시간: 260.90 ms
+메모리: 141 MB, 시간: 236.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 21일 01:58:19
+2025년 01월 21일 02:08:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,16 +3,10 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        StringBuilder sb=new StringBuilder();
-        Arrays.stream(numbers).mapToObj(Integer::toString)
-            .sorted((i,j)->{
-                return Integer.compare(Integer.parseInt(j+i),Integer.parseInt(i+j));
-            })
-            .forEach(i->sb.append(i));
-        while(sb.length()!=0&&sb.charAt(0)=='0'){
-            sb.delete(0,1);
-        }
-        if(sb.length()==0) sb.append('0');
-        return sb.toString();
+        if(Arrays.stream(numbers).allMatch(i->i==0)) return "0"; //"00"이 반환되면 안됨
+        else return Arrays.stream(numbers).mapToObj(Integer::toString)
+            .sorted((i,j)->Integer.compare(Integer.parseInt(j+i),
+                                           Integer.parseInt(i+j)))
+            .collect(Collectors.joining(""));
     }
 }
