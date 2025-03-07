a5facf0268eeceba9f1334f3d8c2938478ac3509
Hyunseung Jeong <129158071+cau20232907@users.noreply.github.com>
2025-03-08 01:27:44 +0900

    [level 3] Title: 상담원 인원, Time: 8.55 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    int[][] arr;
+    public int solution(int k, int n, int[][] reqs) {
+        Map<Integer,List<Request>>reqByType=Arrays.stream(reqs)
+            .map(Request::new)
+            // .sorted((i,j)->Integer.compare(i.time,j.time))
+            .collect(Collectors.groupingBy(r->r.type,Collectors.toList()));
+        int[][] waitingTime=new int[k][n-k+1];
+        arr=waitingTime;
+        //waitingTime[유형][추가 상담원 수]
+        int totalTime=1;
+        PriorityQueue<Integer> finishTimes=new PriorityQueue<>(waitingTime[0].length);
+        for(int i:reqByType.keySet()){
+            List<Request> requests=reqByType.get(i);
+            for(int j=0;j<waitingTime[i].length;j++){
+                totalTime=0;
+                for(int l=0;l<=j;l++) finishTimes.add(0);
+                for(Request request:requests){
+                    int currentTime=finishTimes.poll();
+                    if(currentTime<=request.time)
+                        currentTime=request.time;
+                    else totalTime+=currentTime-request.time;
+                    finishTimes.add(currentTime+request.duration);
+                }
+                waitingTime[i][j]=totalTime;
+                finishTimes.clear();
+                if(totalTime==0) break;
+            }
+        }
+        //상담원별 대기시간 계산 끝
+        //백트래킹
+        return minDFS(0,waitingTime[0].length-1);
+    }
+    private int minDFS(int idx, int left){
+        if(idx+1==arr.length) return arr[idx][left];
+        int value=Integer.MAX_VALUE;
+        for(int i=0;i<=left;i++){
+            value=Math.min(value,arr[idx][i]+minDFS(idx+1,left-i));
+            if(arr[idx][i]==0) break;
+        }
+        return value;
+    }
+    private class Request{
+        //getter 생략
+        final int time;
+        final int duration;
+        final int type;
+        Request(int[] req){
+            this.time=req[0];
+            this.duration=req[1];
+            this.type=req[2]-1;
+        }
+    }
+}
