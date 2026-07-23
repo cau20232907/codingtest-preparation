7d316750659ad3feabc41167c7ab11b62cf03c3f
Hyunseung Jeong <129158071+cau20232907@users.noreply.github.com>
2026-07-24 00:06:46 +0900

    [level 0] Title: l로 만들기, Time: 15.35 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -2,13 +2,6 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(String myString) {
-        return myString.chars()
-            .mapToObj(c -> {
-                if (c < 'l') {
-                    c = 'l';
-                }
-                return new String(Character.toChars(c));
-            })
-            .collect(Collectors.joining());
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
